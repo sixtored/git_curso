bb3fe3860395a5c78de3385ebc238c64c267ad60
sixtored <sixtod@gmail.com>
2020-06-24 15:41:08 -0300

    Modificado titulo de index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include 'Configuracion.php';
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <title>PHP Shopping Cart Tutorial</title>
+    <title>PHP Carrito de compras Tutorial</title>
     <meta charset="utf-8">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
